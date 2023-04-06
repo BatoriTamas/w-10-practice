fc171b0802fd368b2d2666a3795468b42ec5f9b6
BatoriTamas <batori.tamas@gmail.com>
2023-04-06 11:01:11 +0200

    corrections

---
+++ frontend/public/script.js
@@ -0,0 +1,39 @@
+console.log('loaded')
+
+const formComponent = () => `
+  <form>
+    <input type="text" name="name" placeholder="image name">
+    <input type="file" name="file">
+    <button>SEND</button>
+  </form>
+`
+const rootElement = document.querySelector('#root')
+
+rootElement.insertAdjacentHTML('beforeend', formComponent())
+
+const formElement = document.querySelector('form')
+
+formElement.addEventListener('submit', (event) => {
+  event.preventDefault()
+  console.log('Submit')
+
+  const formData = new FormData()
+  formData.append('name', document.querySelector("input[type='text']").value)
+  formData.append('image', document.querySelector("input[type='file']").files[0])
+  fetch('/upload', {
+    method: 'POST',
+    body: formData
+  })
+  .then(res => {
+    if (res.status === 200) {
+      console.log('success')
+      return res.json() // return "pelda"
+    } else {
+      console.log('Oh, something went wrong')
+    }
+  })
+  .then(resData => { // resData = "pelda"
+    rootElement.insertAdjacentHTML('beforeend', `<img src="./public/${resData}.jpg">`)
+  })
+  .catch(error => console.log(error))
+})
